fcce92c33c9a0b53e9840db7fcd274834d33d10d
Rox Niederk <rox.niederkorn@gmail.com>
2023-05-23 20:00:56 -0500

    BrowserRouter config

--- index.jsx
+++ index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { ReactDOM } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
     return (
-        <h1>Hello World!</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+function Home() {
+    return (
+        <h1>Hello, React Router!</h1>
+    )
+}
+
+function About() {
+    return (
+        <h1>About page goes here!🎉</h1>
+    )
+}
+
+
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(<App />);
